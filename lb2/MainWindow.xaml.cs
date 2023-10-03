dad333098264c9b2cd6960ff653725990f3c5a50
Olya Parkhacheva <olya.parhacheva@yandex.ru>
2023-10-03 22:50:53 +0300

    Add Trackball

--- lb2/MainWindow.xaml.cs
+++ lb2/MainWindow.xaml.cs
@@ -32,16 +32,7 @@ namespace lb2
             DockPanel dock = new DockPanel();           
             Content = dock;
 
-            // Create Scrollbar for moving camera
-            ScrollBar scroll = new ScrollBar();
-            scroll.Orientation = Orientation.Horizontal;
-            scroll.Value = -0.2;
-            scroll.Minimum = -0.5;
-            scroll.Maximum = 0.5;
-            scroll.ValueChanged += ScrollBarOnValueChanged;
-            dock.Children.Add(scroll);
-            DockPanel.SetDock(scroll, Dock.Bottom);
-
+            
             // Create Viewport3D for 3D scene
             Viewport3D viewport = new Viewport3D();
             dock.Children.Add(viewport);
@@ -58,16 +49,22 @@ namespace lb2
             ModelVisual3D modvis = new ModelVisual3D();
             modvis.Content = geomod;
             viewport.Children.Add(modvis);
-
+            
             // Create another ModelVisual3D for light
             modvis = new ModelVisual3D();
             modvis.Content = new AmbientLight(Colors.White);
             viewport.Children.Add(modvis);
 
+           
+
+
             // Create the camera
-            cam = new PerspectiveCamera(new Point3D(-1, 0.5, 4),
+            cam = new PerspectiveCamera(new Point3D(0, 0.5, 4),
                          new Vector3D(0, 0, -1), new Vector3D(0, 1.5, 0), 45);
             viewport.Camera = cam;
+            Trackball tr = new Trackball();
+            tr.EventSource = this;
+            cam.Transform = tr.Transform;
         }
         void ScrollBarOnValueChanged(object sender,
                            RoutedPropertyChangedEventArgs<double> args)
@@ -96,10 +93,149 @@ namespace lb2
                 if (i> 0) { 
                     mesh.TriangleIndices.Add(0);
                     mesh.TriangleIndices.Add(i+1);
-                } if (i==count-1) mesh.TriangleIndices.Add(1);
+                } 
+                if (i==count-1) mesh.TriangleIndices.Add(1);
 
             }
             return mesh;
         }
+
+
+        public class Trackball
+        {
+            private FrameworkElement _eventSource;
+            private Point _previousPosition2D;
+            private Vector3D _previousPosition3D = new Vector3D(0, 0, 1);
+
+            private Transform3DGroup _transform;
+            private ScaleTransform3D _scale = new ScaleTransform3D(1, 1, 1);
+            private AxisAngleRotation3D _rotation = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 0);
+
+            public Trackball()
+            {
+
+                _transform = new Transform3DGroup();
+                _transform.Children.Add(_scale);
+                _transform.Children.Add(new RotateTransform3D(_rotation));
+            }
+
+            
+            //     A transform to move the camera or scene to the trackball's
+            //     current orientation and scale.
+            
+            public Transform3D Transform
+            {
+                get { return _transform; }
+            }
+
+            #region Event Handling
+
+            
+            //     The FrameworkElement we listen to for mouse events.
+            
+            public FrameworkElement EventSource
+            {
+                get { return _eventSource; }
+
+                set
+                {
+                    if (_eventSource != null)
+                    {
+                        _eventSource.MouseDown -= this.OnMouseDown;
+                        _eventSource.MouseUp -= this.OnMouseUp;
+                        _eventSource.MouseMove -= this.OnMouseMove;
+                    }
+
+                    _eventSource = value;
+
+                    _eventSource.MouseDown += this.OnMouseDown;
+                    _eventSource.MouseUp += this.OnMouseUp;
+                    _eventSource.MouseMove += this.OnMouseMove;
+                }
+            }
+
+            private void OnMouseDown(object sender, MouseEventArgs e)
+            {
+                Mouse.Capture(EventSource, CaptureMode.Element);
+                _previousPosition2D = e.GetPosition(EventSource);
+                _previousPosition3D = ProjectToTrackball(
+                    EventSource.ActualWidth,
+                    EventSource.ActualHeight,
+                    _previousPosition2D);
+            }
+
+            private void OnMouseUp(object sender, MouseEventArgs e)
+            {
+                Mouse.Capture(EventSource, CaptureMode.None);
+            }
+
+            private void OnMouseMove(object sender, MouseEventArgs e)
+            {
+                Point currentPosition = e.GetPosition(EventSource);
+
+                // Prefer tracking to zooming if both buttons are pressed.
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Track(currentPosition);
+                }
+                else if (e.RightButton == MouseButtonState.Pressed)
+                {
+                    Zoom(currentPosition);
+                }
+
+                _previousPosition2D = currentPosition;
+            }
+
+            #endregion Event Handling
+
+            private void Track(Point currentPosition)
+            {
+                Vector3D currentPosition3D = ProjectToTrackball(
+                    EventSource.ActualWidth, EventSource.ActualHeight, currentPosition);
+
+                Vector3D axis = Vector3D.CrossProduct(_previousPosition3D, currentPosition3D);
+                double angle = Vector3D.AngleBetween(_previousPosition3D, currentPosition3D);
+                Quaternion delta = new Quaternion(axis, -angle);
+
+                // Get the current orientantion from the RotateTransform3D
+                AxisAngleRotation3D r = _rotation;
+                Quaternion q = new Quaternion(_rotation.Axis, _rotation.Angle);
+
+                // Compose the delta with the previous orientation
+                q *= delta;
+
+                // Write the new orientation back to the Rotation3D
+                _rotation.Axis = q.Axis;
+                _rotation.Angle = q.Angle;
+
+                _previousPosition3D = currentPosition3D;
+            }
+
+            private Vector3D ProjectToTrackball(double width, double height, Point point)
+            {
+                double x = point.X / (width / 2);    // Scale so bounds map to [0,0] - [2,2]
+                double y = point.Y / (height / 2);
+
+                x = x - 1;                           // Translate 0,0 to the center
+                y = 1 - y;                           // Flip so +Y is up instead of down
+
+                double z2 = 1 - x * x - y * y;       // z^2 = 1 - x^2 - y^2
+                double z = z2 > 0 ? Math.Sqrt(z2) : 0;
+
+                return new Vector3D(x, y, z);
+            }
+
+            private void Zoom(Point currentPosition)
+            {
+                double yDelta = currentPosition.Y - _previousPosition2D.Y;
+
+                double scale = Math.Exp(yDelta / 100);    // e^(yDelta/100) is fairly arbitrary.
+
+                _scale.ScaleX *= scale;
+                _scale.ScaleY *= scale;
+                _scale.ScaleZ *= scale;
+            }
+        }
+
     }
 }
